b16b2b2ad98f03f61ccfad08197376d7dd815645
iamnana-tyc <nanatyc7@gmail.com>
2025-12-24 01:13:25 +0300

    Implement employee controller

--- EmployeeManagerBackend/src/main/java/com/iamnana/EmployeeManagerBackend/controller/EmployeeController.java
+++ EmployeeManagerBackend/src/main/java/com/iamnana/EmployeeManagerBackend/controller/EmployeeController.java
@@ -1,36 +1,50 @@
 package com.iamnana.EmployeeManagerBackend.controller;
 
-import com.iamnana.EmployeeManagerBackend.entity.Employee;
+import com.iamnana.EmployeeManagerBackend.config.AppConstant;
+import com.iamnana.EmployeeManagerBackend.payload.EmployeeDTO;
+import com.iamnana.EmployeeManagerBackend.payload.EmployeeResponse;
 import com.iamnana.EmployeeManagerBackend.service.EmployeeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
-    @PostMapping("/add")
-    public Employee addEmployee(@RequestBody Employee employee){
-        return employeeService.addEmployee(employee);
-    }
-    @GetMapping("/all")
-    public List<Employee> getAllEmployees(){
-        return employeeService.getAllEmployees();
-    }
-    @GetMapping("/employee/{id}")
-    public Employee getEmployeeById(@PathVariable("id") Long id){
-        return employeeService.getEmployeeById(id);
-    }
-    @DeleteMapping("/employee/{id}")
-    public String deleteEmployeeById(@PathVariable("id") Long id){
-        return employeeService.deleteEmployeeById(id);
-    }
-    @PutMapping("update/{id}")
-    public Employee updateEmployee(@PathVariable("id") Long id, @RequestBody Employee employee){
-        return employeeService.updateEmployee(id, employee);
 
+    @PostMapping("/employees")
+    public ResponseEntity<EmployeeDTO> addEmployee(@Valid @RequestBody EmployeeDTO employeeDTO) {
+        EmployeeDTO employee = employeeService.createEmployee(employeeDTO);
+
+        return new  ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<EmployeeResponse> getAllEmployees(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize
+    ){
+        EmployeeResponse response = employeeService.findAllEmployees(pageNumber,pageSize);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public ResponseEntity<EmployeeDTO> updateEmployee(@Valid @RequestBody EmployeeDTO employeeDTO,
+                                                      @PathVariable Long employeeId){
+        EmployeeDTO employee = employeeService.updateEmployee(employeeDTO,employeeId);
+
+        return new  ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public ResponseEntity<EmployeeDTO> deleteEmployee(@PathVariable Long employeeId){
+        EmployeeDTO employee = employeeService.deleteEmployee(employeeId);
+
+        return new  ResponseEntity<>(employee, HttpStatus.OK);
     }
 }
