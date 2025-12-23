492789ddcd5324a77db67f5d0c29c319786b7254
iamnana-tyc <nanatyc7@gmail.com>
2025-12-24 01:15:42 +0300

    Update employee repo

--- EmployeeManagerBackend/src/main/java/com/iamnana/EmployeeManagerBackend/repository/EmployeeRepository.java
+++ EmployeeManagerBackend/src/main/java/com/iamnana/EmployeeManagerBackend/repository/EmployeeRepository.java
@@ -1,6 +1,6 @@
 package com.iamnana.EmployeeManagerBackend.repository;
 
-import com.iamnana.EmployeeManagerBackend.entity.Employee;
+import com.iamnana.EmployeeManagerBackend.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
