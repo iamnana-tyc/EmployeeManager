4235889d36172cfe0daac4a2f887f8fc634df2d1
iamnana-tyc <nanatyc7@gmail.com>
2025-12-24 01:16:26 +0300

    Class for api response

---
+++ EmployeeManagerBackend/src/main/java/com/iamnana/EmployeeManagerBackend/payload/APIResponse.java
@@ -0,0 +1,14 @@
+package com.iamnana.EmployeeManagerBackend.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private String message;
+    private boolean status;
+
+}
